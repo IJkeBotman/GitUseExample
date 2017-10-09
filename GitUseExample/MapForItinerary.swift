b1168fb5f9d224e2e1e58b379df0f57b746b52c4
IJke Botman <ijke.botman@gmail.com>
2017-10-09 16:55:55 +0200

    Add new map-for-itinerary feature.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by IJke Botman on 09/10/2017.
+//  Copyright © 2017 Overscope. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
